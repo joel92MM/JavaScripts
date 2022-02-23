ba5cc6233de51438fc5ba7b85c505a9051936c86
joel92MM <joel_tf_92@hotmail.com>
2022-02-23 01:06:49 +0000

    Comienzo del ejercicio helado.js

---
+++ Condicionales/js/helados.js
@@ -0,0 +1,31 @@
+/* 
+ellos quieren comrpar el gelado mas caro que puedan con la plta que tienen:
+
+roberto tiene 1,5€
+pedro tiene 1,7€
+cofla tiene 3€
+
+los precios de helados son los siguientes;
+
+palitos de helado de agua : 0.6
+palitos de helado de crema : 1
+bombon helado de marca heladix : 1.6
+bombon helado de marca heladovich: 1.7
+bombon helado de marca helardo :1.8
+potecito de heldo confites:2.9
+pote de 1/4kg:2.9
+
+
+ingrresas el dinero que tiene cada uno mostrar helado mas caro
+
+si hay 2 o mas con el mismo precio mostrar ambos
+cofla quiere saber cuanto es el vuelto */
+
+//pedimos el dinero a cada persona
+dineroRoberto=parseInt(prompt("¿cuanto dinero tienes roberto?"));
+dineroCofla=parseInt(prompt("¿cuanto dinero tienes cofla?"));
+dineroPedro=parseInt(prompt("¿cuanto dinero tienes pedro?"));
+
+document.write(`el dinero de roberto es ${dineroRoberto}<br/>`);
+document.write(`el dinero de cofla es ${dineroCofla}<br/>`);
+document.write(`el dinero de pedro es ${dineroPedro}<br/>`);
---
+++ Condicionales/js/index.js
@@ -0,0 +1,18 @@
+/* sentencias if/else */
+
+if (103<=140) {
+    alert("se ejecuta");
+}else{
+    alert("no se ejecuta")
+}
+
+/* sentencias if/else if /else */
+nombre ="pe";
+if (nombre=="joel") {
+    alert(`su nombre es${nombre}`);
+}else if(nombre=="sara"){
+    alert(`su nombre es${nombre}`)
+}else{
+    alert(`su nombre es otro`)
+
+}
